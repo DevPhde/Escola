7b79b99b4802e7fcc782fb40eb455f9ad3737ad5
Diego B <dbsmendes@gmail.com>
2023-02-03 17:45:15 -0300

    Student Info - Adjust

--- src/routes/StudentInfo.jsx
+++ src/routes/StudentInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect, useLayoutEffect } from 'react';
 import { AxiosApi } from "../services/RequisitionAPI"
 import profile from "../assets/profile.jpg"
 import { Student } from '../entities/Student';
@@ -9,23 +9,26 @@ const StudentInfo = () => {
     const [isEditing, setIsEditing] = useState(false);
     const [deletingStudent, setDeletingStudent] = useState(false)
 
+    const [dataState, setDataState] = useState(undefined)
+
     const [name, setName] = useState(data.nome);
     const [birthday, setBirthday] = useState('');
     const [cpf, setCpf] = useState('');
     const [registration, setRegistration] = useState('');
 
-    const [dataState, setDataState] = useState(undefined)
-    
 
-    useEffect(() => {
+
+    useLayoutEffect(() => {
         async function requisitionInfo() {
             try {
                 const connection = await AxiosApi.Get(window.location.pathname)
                 setData(connection.data)
                 setDataState(connection.data)
+                console.log(dataState)
+                console.log(name)
                 setIsLoading(false)
             } catch (error) {
-                alert(error)
+                alert('Erro inesperado, tente novamente mais tarde.')
             }
 
         }
@@ -66,68 +69,69 @@ const StudentInfo = () => {
 
     }
 
-    async function handleDelete(){
+    async function handleDelete() {
         let getInfo = await AxiosApi.Get(window.location.pathname)
         getInfo = getInfo.data
-
+        const newInfos = new Student(name, birthday, cpf, registration, getInfo.turma)
+        console.log(newInfos)
         // const connection = await AxiosApi.Put(window.location.pathname)
     }
-    console.log(name)
+    console.log(dataState)
     if (dataState === undefined) {
         return <p>Carregando...</p>;
-      }
-      else {
-         
-      
-    return (
-        <div>
-            {isEditing ? (
-                <div className='mb-5'>
-                    <div className='mb-5 text-center'>
-                        <img src={profile} alt="" />
-                    </div>
-                    <div className='d-flex flex-column mb-3'>
-                        <div className="form-floating mb-3">
-                            <input type="text" className="form-control border-secondary w-50" value={name} onChange={e => setName(e.target.value)} placeholder="Nome Completo" />
-                            <label htmlFor='floatingFullName'>Nome Completo</label>
-                        </div>
-                        <div className="form-floating mb-3">
-                            <input type="text" className="form-control border-secondary w-50" value={birthday} onChange={e => setBirthday(e.target.value)} placeholder="Data de Nascimento" />
-                            <label htmlFor='floatingFullName'>Data de Nascimento</label>
-                        </div>
-                        <div className="form-floating mb-3">
-                            <input type="text" className="form-control border-secondary w-50" value={cpf} onChange={e => setCpf(e.target.value)} placeholder="CPF" />
-                            <label htmlFor='floatingFullName'>CPF</label>
-                        </div>
-                        <div className="form-floating mb-3">
-                            <input type="text" className="form-control border-secondary w-50" value={registration} onChange={e => setRegistration(e.target.value)} placeholder="Matrícula" />
-                            <label htmlFor='floatingFullName'>Matrícula</label>
-                        </div>
-                    </div>
-                    <div>
-                        <button className='btn-dark btn m-1' onClick={handleSaveClick}>Salvar</button>
-                        <button className='btn-dark btn m-1' onClick={handleCancelClick}>Cancelar</button>
-                    </div>
+    }
+    else {
 
-                </div>
-            ) : (
-                <div className='mb-5'>
-                    <div className='text-center mb-5'>
-                        <img src={profile} alt="" />
-                    </div>
-                    <p>Nome Completo: {data.nome}</p>
-                    <p>Data de Nascimento: {data.dataNascimento}</p>
-                    <p>CPF: {data.cpf}</p>
-                    <p>Matrícula: {data.matricula}</p>
-                    <p>Turma: {!data.turma ? "Aluno não matriculado em nenhuma turma." : data.turma}</p>
-                    <button className='btn btn-dark' onClick={handleEditClick}>Editar Aluno</button>
-                    <button className='btn btn-dark text-end' onClick={handleDeleteStudent}>Deletar Cadastro</button>
 
-                </div>
-            )}
-        </div>
-    );
-            }
+        return (
+            <div>
+                {isEditing ? (
+                    <div className='mb-5'>
+                        <div className='mb-5 text-center'>
+                            <img src={profile} alt="" />
+                        </div>
+                        <div className='d-flex flex-column mb-3'>
+                            <div className="form-floating mb-3">
+                                <input type="text" className="form-control border-secondary w-50" value={name} onChange={e => setName(e.target.value)} placeholder="Nome Completo" />
+                                <label htmlFor='floatingFullName'>Nome Completo</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input type="text" className="form-control border-secondary w-50" value={birthday} onChange={e => setBirthday(e.target.value)} placeholder="Data de Nascimento" />
+                                <label htmlFor='floatingFullName'>Data de Nascimento</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input type="text" className="form-control border-secondary w-50" value={cpf} onChange={e => setCpf(e.target.value)} placeholder="CPF" />
+                                <label htmlFor='floatingFullName'>CPF</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input type="text" className="form-control border-secondary w-50" value={registration} onChange={e => setRegistration(e.target.value)} placeholder="Matrícula" />
+                                <label htmlFor='floatingFullName'>Matrícula</label>
+                            </div>
+                        </div>
+                        <div>
+                            <button className='btn-dark btn m-1' onClick={handleSaveClick}>Salvar</button>
+                            <button className='btn-dark btn m-1' onClick={handleCancelClick}>Cancelar</button>
+                        </div>
+
+                    </div>
+                ) : (
+                    <div className='mb-5'>
+                        <div className='text-center mb-5'>
+                            <img src={profile} alt="" />
+                        </div>
+                        <p>Nome Completo: {data.nome}</p>
+                        <p>Data de Nascimento: {data.dataNascimento}</p>
+                        <p>CPF: {data.cpf}</p>
+                        <p>Matrícula: {data.matricula}</p>
+                        <p>Turma: {!data.turma ? "Aluno não matriculado em nenhuma turma." : data.turma}</p>
+                        <button className='btn btn-dark' onClick={handleEditClick}>Editar Aluno</button>
+                        <button className='btn btn-dark text-end' onClick={handleDeleteStudent}>Deletar Cadastro</button>
+
+                    </div>
+                )}
+            </div>
+        );
+    }
 };
 
 export default StudentInfo;
