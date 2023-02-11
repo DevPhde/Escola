e3b4733c497a6fa89ab8250332760f2efd152765
Isabela Alves <isabelaalvesc17@gmail.com>
2023-02-11 17:20:43 -0300

    header
    icones

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ function Footer() {
     return (
         <footer>
             <div className='border p-3'>
-                <p className='mt-2 text-center icones'>
-                    <a href="https://pt-br.facebook.com/"><i class="text-center mt-2 font fa fa-facebook-f"></i></a>
-                    <a href="https://pt-br.facebook.com/"><i class="text-center mt-2 font fa fa-facebook-f"></i></a>
-                    <a href="https://pt-br.facebook.com/"><i class="text-center mt-2 font fa fa-facebook-f"></i></a>
-                    <a href="https://pt-br.facebook.com/"><i class="text-center mt-2 font fa fa-facebook-f"></i></a>
+                <p className='text-center mt-2 font'>
+                    <a href="https://pt-br.facebook.com/" className='icones font'><i class="text-center mt-2 fa fa-facebook-f"></i></a>
+                    <a href="https://pt-br.facebook.com/" className='icones font'><i class="fa fa-instagram"></i></a>
+                    <a href="https://pt-br.facebook.com/" className='icones font'><i class="material-icons font">mail</i></a>
+                    <a href="https://pt-br.facebook.com/" className='icones font'><i class="fa fa-whatsapp"></i></a>
                 </p>
                 <h5 className='text-center mt-2 font'>Projeto Módulo 3 - React</h5>
                 <p className='mt-2 text-center font'>© 2022 Copyright: Squad 2 - Programadores Cariocas</p>
