8f872be744ddc666279edff49bd1fab53b6a8cf0
Diego B <dbsmendes@gmail.com>
2023-02-12 10:27:47 -0300

    Adjust requisition links

--- src/services/RequisitionAPI.js
+++ src/services/RequisitionAPI.js
@@ -3,18 +3,17 @@ import axios from "axios"
 export class AxiosApi {
 
    static async Get(path) {
-        return await axios.get(`http://localhost:3000${path}`)
-        // return await axios.get(`https://hotel-9crs.onrender.com${path}`)
+        return await axios.get(`https://hotel-9crs.onrender.com${path}`)
     }
     static async Post(path, body) {
-        return await axios.post(`http://localhost:3000${path}`,
+        return await axios.post(`https://hotel-9crs.onrender.com${path}`,
         body)
     }
     static async Put(path, body) {
-        return await axios.put(`http://localhost:3000${path}`,
+        return await axios.put(`https://hotel-9crs.onrender.com${path}`,
         body)
     }
     static async Delete(path) {
-        return await axios.delete(`http://localhost:3000${path}`)
+        return await axios.delete(`https://hotel-9crs.onrender.com${path}`)
     }
 }
