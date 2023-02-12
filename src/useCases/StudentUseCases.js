69fb0c20b4ed80e10bff35288578223fc972737b
Diego B <dbsmendes@gmail.com>
2023-02-11 21:29:02 -0300

    info (student, teacher) - improve code and adjusted CRUD logic

--- src/useCases/StudentUseCases.js
+++ src/useCases/StudentUseCases.js
@@ -14,17 +14,28 @@ export class StudentUseCases {
         const connection = await AxiosApi.Post('/alunos', student)
         return connection.status == 201 ? "Aluno Criado com sucesso!" : "Erro interno, Tente novamente mais tarde.(error code: 15L SUC)"
     }
-    static async DeleteStudent(id){
+    static async DeleteStudent(id) {
+        const connection = await AxiosApi.Get(id)
+        if (connection.data.turma) {
+            const classRoom = await AxiosApi.Get(`/turmas?turma=${connection.data.turma}`)
+            const updateClassRoom = classRoom.data['0']
+            let index = updateClassRoom.alunos.findIndex(aluno => aluno.id === connection.data.id);
+
+            if (index !== -1) {
+                updateClassRoom.alunos.splice(index, 1);
+            }
+            await AxiosApi.Put(`/turmas/${classRoom.data['0'].id}`, updateClassRoom)
+        }
         const deleteInfos = await AxiosApi.Delete(id)
         return deleteInfos.status == 200 ? 'Cadastro Excluído' : 'Erro interno, tente novamente mais tarde.(error code: 19L SUC)'
     }
     static async EditStudent(id, student) {
         student.id = id
-        if(student.turma) {
+        if (student.turma) {
             const connection = await AxiosApi.Get(`/turmas/?turma=${student.turma}`)
             const updateClassRoom = connection.data['0']
             updateClassRoom.alunos.forEach((aluno, index) => {
-                if(aluno.id === id) {
+                if (aluno.id === id) {
                     updateClassRoom.alunos[index] = student;
                 }
             });
