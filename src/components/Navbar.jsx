dfeeb4497366c198a479400a8e24e9bdd200ecdf
Diego B <dbsmendes@gmail.com>
2023-02-13 11:55:33 -0300

    navBar - added responsive NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,35 @@
 import { Link } from "react-router-dom"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-function Navbar() {
+function HeaderNav() {
     return (
-        <nav>
-            <div>
-                <Link className="m-4 link__router" to="/"><button className="btn btn-light button"><h1>AGAPT</h1></button></Link>            </div>
-            <div>
-                <Link className="m-4 link__router" to="/"><button className="btn btn-light button">HOME</button></Link>
-                <Link className="m-1 link__router" to="/cadastro"><button className="btn btn-light button">CADASTRO</button></Link>
-            </div>
-        </nav>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+        <Link className="m-4 link__router" to="/"><button className="bg-dark button"><h4>AGAPT</h4></button></Link>            
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link><Link className="m-4 link__router" to="/"><button className=" bg-dark button">HOME</button></Link></Nav.Link>
+              <Nav.Link><Link className="m-1 link__router" to="/cadastro"><button className="bg-dark button">CADASTRO</button></Link></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     )
 }
 
-export default Navbar;
+export default HeaderNav;
+
+
+
+       // <nav className="navbar navbar-expand-lg">
+        //     <div>
+        //         <Link className="m-4 link__router" to="/"><button className="button"><h1>AGAPT</h1></button></Link>            
+        //      </div>
+        //     <div>
+        //         <Link className="m-4 link__router" to="/"><button className="btn btn-light button">HOME</button></Link>
+        //         <Link className="m-1 link__router" to="/cadastro"><button className="btn btn-light button">CADASTRO</button></Link>
+        //     </div>
+        // </nav>
