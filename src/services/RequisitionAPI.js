553fadc4450fe6c9c6261dd9fb41f894def13e2e
Diego B <dbsmendes@gmail.com>
2023-02-16 03:02:10 -0300

    Adjusted link of requisitions

--- src/services/RequisitionAPI.js
+++ src/services/RequisitionAPI.js
@@ -3,17 +3,17 @@ import axios from "axios"
 export class AxiosApi {
 
    static async Get(path) {
-        return await axios.get(`http://localhost:3000${path}`)
+        return await axios.get(`https://agapt.onrender.com${path}`)
     }
     static async Post(path, body) {
-        return await axios.post(`http://localhost:3000${path}`,
+        return await axios.post(`https://agapt.onrender.com${path}`,
         body)
     }
     static async Put(path, body) {
-        return await axios.put(`http://localhost:3000${path}`,
+        return await axios.put(`https://agapt.onrender.com${path}`,
         body)
     }
     static async Delete(path) {
-        return await axios.delete(`http://localhost:3000${path}`)
+        return await axios.delete(`https://agapt.onrender.com${path}`)
     }
 }
